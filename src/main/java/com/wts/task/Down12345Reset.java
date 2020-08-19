4284ca5d170b94de7082f45d57fe90219e376c41
nelso <nelsonnick@126.com>
2020-08-19 22:56:45 +0800

    微信发送

--- src/main/java/com/wts/task/Down12345Reset.java
+++ src/main/java/com/wts/task/Down12345Reset.java
@@ -2,12 +2,18 @@ package com.wts.task;
 
 import com.jfinal.kit.PropKit;
 import com.wts.util.ParamesAPI;
+import me.chanjar.weixin.common.bean.result.WxMediaUploadResult;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.cp.api.impl.WxCpServiceImpl;
 import me.chanjar.weixin.cp.bean.WxCpMessage;
 import me.chanjar.weixin.cp.config.impl.WxCpDefaultConfigImpl;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.wts.util.util12345.getDoc;
 import static com.wts.util.util12345.getPageUrl;
@@ -43,5 +49,25 @@ public class Down12345Reset implements Runnable {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception{
+        WxCpDefaultConfigImpl config = new WxCpDefaultConfigImpl();
+        config.setCorpId(ParamesAPI.corpId);      // 设置微信企业号的appid
+        config.setCorpSecret(ParamesAPI.secret);  // 设置微信企业号的app corpSecret
+        config.setAgentId(1000002);     // 设置微信企业号应用ID
+        config.setToken("...");       // 设置微信企业号应用的token
+        config.setAesKey("...");      // 设置微信企业号应用的EncodingAESKey
+        WxCpServiceImpl wxCpService = new WxCpServiceImpl();
+        wxCpService.setWxCpConfigStorage(config);
+        File file = new File("D:\\TemplateDoc.docx");
+        WxMediaUploadResult res = wxCpService.getMediaService().upload("file",file);
+        WxCpMessage message = WxCpMessage
+                .FILE()
+                .toUser("WangTianShuo")
+                .agentId(1000002)
+                .mediaId(res.getMediaId())
+                .build();
+        wxCpService.messageSend(message);
     }
 }
